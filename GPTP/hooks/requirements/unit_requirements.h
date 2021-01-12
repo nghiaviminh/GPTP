a220e904c1490d889b2ce3d74f11583836eef886
Milestone <56237952+milestone-dev@users.noreply.github.com>
2021-01-12 02:21:02 +0100

    Hook TTAllowed (0x0046E1C0)
    Known issues: Opcodes containing "Or" do not function

--- GPTP/hooks/requirements/unit_requirements.h
+++ GPTP/hooks/requirements/unit_requirements.h
@@ -1,1 +1,10 @@
 #pragma once
+#include <SCBW/api.h>
+
+namespace hooks {
+
+	s8 UnitCreateAllowed(u16 unitId, CUnit* unit, int playerID);//0046E1C0	
+
+	void injectUnitCreateRequirementsWrapper();
+
+}
